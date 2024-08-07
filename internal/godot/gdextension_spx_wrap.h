c3210b12946b48c42cc9f8fa34afc0ee103caee2
jiepengtan <jiepengtan@gmail.com>
2024-08-07 19:19:51 +0800

    Add godot extention functions

--- internal/godot/gdextension_spx_wrap.h
+++ internal/godot/gdextension_spx_wrap.h
@@ -6,20 +6,56 @@
 #include <stdlib.h>
 #include "gdextension_interface.h"
 #include "gdextension_spx_interface.h"
-
 #ifndef __cplusplus
 typedef uint32_t char32_t;
 typedef uint16_t char16_t;
 #endif
 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifdef REAL_T_IS_DOUBLE
+typedef double real_t;
+#else
+typedef float real_t;
+#endif
 
+typedef struct {
+    real_t x;
+    real_t y;
+    real_t z;
+    real_t w;
+} Vector4;
+
+typedef struct {
+    real_t x;
+    real_t y;
+    real_t z;
+} Vector3;
+
+typedef struct {
+    real_t x;
+    real_t y;
+} Vector2;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float w;
+} Color;
+
+typedef struct {
+	Vector2 position;
+	Vector2 size;
+} Rect2;
 
 #ifdef __cplusplus
 }
 #endif
+
+#include "gdextension_spx_ext.h"
 
 #endif // GDEXTENSION_SPX_WRAP_H
