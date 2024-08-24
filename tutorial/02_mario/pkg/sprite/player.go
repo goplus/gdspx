933874761006a20e78f93751ecae0a5d41a7fa6f
jiepengtan <jiepengtan@gmail.com>
2024-08-24 14:38:09 +0800

    Add basic movement

--- tutorial/02_mario/pkg/sprite/player.go
+++ tutorial/02_mario/pkg/sprite/player.go
@@ -4,26 +4,19 @@ import (
 	. "godot-ext/gdspx/pkg/engine"
 )
 
-type Aircraft struct {
+type Player struct {
 	Sprite
 	timer      float32
 	moveSpeed  float32
 	OnDieEvent *Event0
 }
 
-func (pself *Aircraft) OnStart() {
+func (pself *Player) OnStart() {
 	pself.moveSpeed = 600
 	pself.OnDieEvent = NewEvent0()
 }
-func (pself *Aircraft) OnUpdate(delta float32) {
+func (pself *Player) OnUpdate(delta float32) {
 	pself.timer += delta
-	if pself.timer > 0.3 {
-		pself.timer = 0
-		bullet := CreateSprite[Bullet]()
-		pos := pself.GetPosition()
-		bullet.SetPosition(Vec2{pos.X, pos.Y + 150})
-	}
-
 	if InputMgr.GetKey(KeyCode.W) {
 		pself.Move(0, pself.moveSpeed*delta)
 	}
@@ -39,6 +32,6 @@ func (pself *Aircraft) OnUpdate(delta float32) {
 }
 
 
-func (pself *Aircraft) OnHit() {
+func (pself *Player) OnHit() {
 	pself.OnDieEvent.Trigger()
 }
