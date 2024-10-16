a00e0d5955c7fcec452f05a6ac750382316ddc5d
jiepengtan <jiepengtan@gmail.com>
2024-10-16 15:04:16 +0800

    Fxied gdspx export web by makefile tool

--- tools/build_game.sh
+++ tools/build_game.sh
@@ -6,7 +6,15 @@ CURRENT_PATH=("$PWD")
 # Default values
 EXPORT_WEB=false
 PROJECT_PATH="tutorial/01_aircraft"
-GODOT="./godot/bin/godot.linuxbsd.editor.dev.x86_64"
+if [ "$OS" = "Windows_NT" ]; then
+    GODOT="./godot/bin/godot.windows.editor.dev.x86_64"
+elif [ "$OS" = "Linux" ]; then
+    GODOT="./godot/bin/godot.linuxbsd.editor.dev.x86_64"
+else
+    GODOT="./godot/bin/godot.darwin.editor.dev.x86_64"
+fi
+
+
 PLATFORM=""
 EDITOR=false
 LIB_NAME=false
@@ -24,7 +32,7 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-GD_PROJ_DIR="$PROJECT_PATH/project" 
+GD_PROJ_DIR="$PROJECT_PATH" 
 GD_LIB_DIR=$GD_PROJ_DIR/.godot
 LIB_DIR=$GD_PROJ_DIR/lib
 
@@ -33,35 +41,36 @@ rm -rf $LIB_DIR
 rm -f $GD_LIB_DIR/extension_list.cfg
 rm -f $GD_PROJ_DIR/gdspx.gdextension 
 
-
 mkdir -p $GD_LIB_DIR
 mkdir -p $LIB_DIR
 
+TEMPLATE_DIR=$CURRENT_PATH/cmd/gdspx/pkg/impl/template/
+cp $TEMPLATE_DIR/export_presets.cfg $GD_PROJ_DIR/export_presets.cfg 
+
 if [ "$PLATFORM" = "web" ]; then
     # update extension list
-    mkdir -p ./builds/games
+    mkdir -p $PROJECT_PATH/.builds/web
 
     # Build wasm
     cd "$PROJECT_PATH" || { echo "Project path not found"; exit 1; }
     echo "Building Go wasm"
-    GOOS=js GOARCH=wasm go build -o "$CURRENT_PATH/builds/games/gdspx.wasm"
+    GOOS=js GOARCH=wasm go build -o ".builds/web/gdspx.wasm"
     cd $CURRENT_PATH
 
     # Export to web if enabled
     if [ "$EXPORT_WEB" = true ]; then
         echo "================ EXPORT_WEB ====================="
-        $GODOT --headless --quit --path $GD_PROJ_DIR --export-debug "Web" "$CURRENT_PATH/builds/games/index.html"
+        $GODOT --headless --quit --path $GD_PROJ_DIR --export-debug Web 
     fi
 elif  [ "$PLATFORM" = "" ]; then #pc
     # update extension list
-    echo "res://gdspx.gdextension" > $GD_LIB_DIR/extension_list.cfg
-    TEMPLATE_DIR=$CURRENT_PATH/cmd/gdspx/template/project/gdspx.gdextension
-    cp $TEMPLATE_DIR $GD_PROJ_DIR/gdspx.gdextension 
+    cp $TEMPLATE_DIR/extension_list.cfg $GD_LIB_DIR/extension_list.cfg 
+    cp $TEMPLATE_DIR/gdspx.gdextension $GD_PROJ_DIR/gdspx.gdextension 
 
     cd $PROJECT_PATH 
-	CGO_ENABLED=1 go build -buildmode=c-shared -o ./project/lib/$LIB_NAME main.go
+	CGO_ENABLED=1 go build -buildmode=c-shared -o ./lib/$LIB_NAME main.go
     cd $CURRENT_PATH
-    
+    echo $GODOT --debug  --path $GD_PROJ_DIR --editor 
     if [ "$EDITOR" = true ]; then
         $GODOT --debug  --path $GD_PROJ_DIR --editor 
     else 
--- tools/start_server.sh
+++ tools/start_server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-pkill -f gdspx_web_server
 
+if [ "$OS" = "Windows_NT" ]; then
+    echo -e "taskkill /F /IM python.exe\ntaskkill /F /IM pythonw.exe" > temp_kill.bat
+    cmd.exe /C temp_kill.bat
+    rm temp_kill.bat
+else 
+    pkill -f gdspx_web_server.py
+fi
+
+PROJ_PATH=$1
 echo "gdgo server running at http://localhost:8005"
-python ./tools/gdspx_web_server.py -r ../builds/games -p 8005  &
+python ./tools/gdspx_web_server.py -r ../$PROJ_PATH/.builds/web -p 8005  &
