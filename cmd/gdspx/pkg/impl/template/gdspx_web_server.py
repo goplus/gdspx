2e3e73e95cfd9121e970e0ae0085e0a8e169504c
jiepengtan <jiepengtan@gmail.com>
2024-10-17 09:54:16 +0800

    Run test webserver in "no cache" mode for test

--- cmd/gdspx/pkg/impl/template/gdspx_web_server.py
+++ cmd/gdspx/pkg/impl/template/gdspx_web_server.py
@@ -21,9 +21,12 @@ class DualStackServer(HTTPServer):
 
 class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
         self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
         super().end_headers()
     def log_message(self, format, *args):
         pass  # 通过覆盖 log_message 方法来屏蔽日志输出
